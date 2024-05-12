fe1b44232c57f30925e56d370802eac9625991f0
Trainee1 <trainee1@arvandfanpars.com>
2024-05-12 16:59:20 +0330

    add header

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -16,19 +16,14 @@ const Admin = () => {
     }
     return (
         <div className="w-full bg-white min-h-screen font-yekan">
-            <div className="w-full flex items-center justify-center bg-[#0A6847] py-3 gap-4 shadow-xl">
-                <button className="text-xl text-white">
-                    <TbBrush />
-                </button>
-                <button className="text-xl text-white"><SiTestcafe />
-                </button>
-                <button className="text-xl text-white"></button>
-            </div>
+            
             <MenuCreateSurvey setSorvay={setSurvay} survay={survay} />
             {
                 showEditSurvayMenu && <MenuEditSurvay survay={survay[index]} setSurvay={setSurvay} open={showEditSurvayMenu} setOpen={setShowEditSurvayMenu} />
             }
+            <div className="w-full fixed bottom-0 py-4 px-3 bg-gray-100 shadow-md ">
 
+            </div>
         </div>
     )
 }
